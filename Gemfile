296e0325713e0db8a87323985dfef4879187a785
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-10-04 15:11:37 -0700

    Add MentionQueue class for queueing tweets.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 # Gemfile for StreamReader
 source "http://rubygems.org"
 
+gem 'hiredis'
+gem 'redis'
+gem 'resque'
 gem "yajl-ruby"
 gem "tweetstream"
---
+++ lib/mention_queue.rb
@@ -0,0 +1,31 @@
+require 'hiredis'
+require 'redis'
+require 'resque'
+require 'uri'
+require 'pry'
+
+class MentionQueue
+  def initialize redis_uri, channel_id
+    Resque.redis = redis_from redis_uri
+    @channel_id = channel_id
+  end
+
+  # Add the tweet directly to the resque queue bypassing the public API since it
+  # checks that the constants are defined in the running context whereas this
+  # does not.
+  def enqueue tweet_hash
+    puts "Enqueing #{tweet_hash.id}"
+    args = [ 'twitter', tweet_hash, @channel_id ]
+    Resque.push 'mention', class: 'Aji::Queues::Mention::Process', args: args
+  end
+
+  def redis_from uri
+    uri = URI uri
+    Redis.new(
+      :host => uri.host,
+      :port => uri.port,
+      :password => uri.password,
+      :db => uri.path[1..-1]
+    )
+  end
+end
