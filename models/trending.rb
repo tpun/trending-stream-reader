549130f8eceef1f558077738d73aefc279490f98
Thomas Pun <thomas@nowbox.com>
2012-02-27 17:52:07 -0800

    key should be an instance var

--- models/trending.rb
+++ models/trending.rb
@@ -1,19 +1,20 @@
 module Aji
   class Trending
+    attr_reader :video_source, :key
     def initialize video_source='youtube'
       @video_source = video_source
-    end
-
-    def key
-      "trending::#{@video_source}"
+      @key = "trending::#{@video_source}"
     end
 
     def promote_video mention
-      Aji.redis.zincrby key, mention.significance, mention.video.uid
+      Aji.redis.zincrby @key, mention.significance, mention.video.uid
     end
 
     def video_uids limit=0
-      (Aji.redis.zrevrange key, 0, (limit-1)).map(&:to_i)
+      (Aji.redis.zrevrange @key, 0, (limit-1)).map(&:to_i)
+    end
+
+    def refresh
     end
   end
 end
